96732fdeab61b6f938d29f8dee513e7d4f62db0d
LicSis <gaston.caceres@educacionadventista.org.ar>
2024-06-12 15:00:49 -0300

    Organizando Repositorio

---
+++ trabajos_varios/inicio/principio.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operaciones</title>
+</head>
+<body>
+    <form action="principio.php" method="post">
+        
+    </form>
+    <?php
+        $num1 = 1.98;
+        $num2 = 34.53;
+        $suma = $num1 + $num2;
+        echo '<h1>La suma es: '.$suma. '</h1>';
+        /* if (condition) {
+            # code...
+         }    */
+    ?>
+</body>
+</html>
